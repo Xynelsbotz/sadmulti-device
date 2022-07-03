f22a455df6cded8b4809b06a1ce18f3495909876
Xynels Design <107124409+Xynelsbotz@users.noreply.github.com>
2022-07-03 16:34:27 +0700

    Update weebs-loli.js

--- plugins/weebs-loli.js
+++ plugins/weebs-loli.js
@@ -5,7 +5,7 @@ let sites = ['sb', 'kn', 'kc']
 let handler = async (m, { conn, usedPrefix, command }) => {
 	let res = await Booru.search(sites.getRandom(), ['loli'], { random: true })
 	let url = res[0].fileUrl
-	conn.sendButton(m.chat, 'Random Image Loli', await shortUrl(url), url, [['Next', usedPrefix + command]], m)
+	conn.sendButton(m.chat, 'Random Image Loli\n\nSagne sama loli banh? lari ada pedo:v', await shortUrl(url), url, [['Next', usedPrefix + command]], m)
 }
 handler.help = ['loli']
 handler.tags = ['weebs']
