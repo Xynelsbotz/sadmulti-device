fa03b5f6826058eaa561218093a1f8d8df453429
Xynels Design <107124409+Xynelsbotz@users.noreply.github.com>
2022-07-03 16:28:28 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://github.com/FahriAdison/Sad-Multi-Device*\nSubscribe Untuk Mengetahui Update Selanjutnya:bit.ly/Papah-Chan\nSilahkan follow github saya dibawah ,Terimakasih`
+let str = `*CHAT OWNER*\nSubscribe Untuk Mengetahui Update Selanjutnya:https://bit.ly/xynelsdesign\nSilahkan follow github saya dibawah ,Terimakasih`
 conn.sendButtonDoc(m.chat, str, wm, 'Thanks','Bilek', ftextt, m)
 }
 handler.command = handler.help = ['sc']
