96ff67c38ea20386759a60cec58c5728e7fd27f1
Xynels Design <107124409+Xynelsbotz@users.noreply.github.com>
2022-10-12 20:46:22 +0700

    Update general-menu2.js

--- plugins/general-menu2.js
+++ plugins/general-menu2.js
@@ -1,11 +1,7 @@
 import moment from 'moment-timezone'
-import jimp from 'jimp'
-import fs from 'fs'
-import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, __dirname, text, command }) => {
 let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
 let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
-let vn = './tante-tante.mp3'
 conn.sendHydrated(m.chat, '*『 sᴀʏᴜ ᴏɢɪᴡᴀʀᴀ ʙᴏᴛᴢ ʙʏ xʏɴᴇʟs 』*\n\nʙɪᴏᴅᴀᴛᴀ xʏɴᴇʟs :\nhttps://bit.ly/3I7I8xR', `ғᴏʟʟᴏᴡ ɪɴsᴛᴀɢʀᴀᴍ : @xʏɴᴇʟs.ᴅᴇsɪɢɴ\nsᴜʙsᴄʀɪʙᴇ ᴄʜᴀɴɴᴇʟ ᴏᴡɴᴇʀ: xʏɴᴇʟs ᴅᴇsɪɢɴ\n\nʜᴀɪ,ᴀᴋᴜ ᴀᴅᴀʟᴀʜ ʙᴏᴛ ʏᴀɴɢ ᴅɪʙᴇʀɪ ɴᴀᴍᴀ sᴀʏᴜ ᴏɢɪᴡᴀʀᴀ ʙᴏᴛᴢ,ᴀᴋᴜ ᴅɪ ᴋᴇᴍʙᴀɴɢᴋᴀɴ ᴏʟᴇʜ ꜱᴇꜱᴇᴏʀᴀɴɢ ʏᴀɴɢ ʙᴇʀɴᴀᴍᴀ xʏɴᴇʟs [ɴᴇʟsᴇɴ],ʙᴇʟɪᴀᴜ ᴍᴀꜱɪʜ ᴘᴇᴍᴜʟᴀ ᴅᴀɴ ᴍᴀꜱɪʜ ʙᴇʟᴀᴊᴀʀ ʙᴀɴʏᴀᴋ ʜᴀʟ,ᴅᴀɴ ᴛᴜᴊᴜᴀɴɴʏᴀ ʜᴀɴʏᴀ ɪɴɢɪɴ ᴛᴀᴜ ᴀᴘᴀ ʏᴀɴɢ ɪɴɢɪɴ ᴅɪᴋᴇᴛᴀʜᴜɪ ᴅɪ ᴅᴜɴɪᴀ ʙᴏᴛ,\n\ngithub:\nhttps://github.com/Xynelsbotz\n\n╭━━━[🍃𝐒𝐀𝐘𝐔 𝐎𝐆𝐈𝐖𝐀𝐑𝐀🍃]\n┊Owner : Xynels Design\n┊Instagram : Xynels.design\n┊Youtube : XYNELS DESIGN\n╰═┅═━––––––––––––––––๑\nsɪʟᴀʜᴋᴀɴ ᴋʟɪᴋ ᴍᴇɴᴜ ᴜɴᴛᴜᴋ ᴍᴇɴᴜ sᴇʟᴇɴɢᴋᴀᴘɴʏᴀ\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), "./media/broadcast.jpg", 'https://bit.ly/xynelsdesign', 'ʏᴏᴜᴛᴜʙᴇ xʏɴᴇʟs', null, null, [
 [`ᴍᴇɴᴜ📑`, '.m'],
 [`\ʀᴜʟᴇs⚠️`, '.mr']
